88351a89194e02a8c52564f6a9790c29e980f612
kadrianne <kristine.a.du@gmail.com>
2020-04-27 15:31:03 -0600

    Add game button component and onClick event.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Board from './components/Board';
 
 class App extends React.Component {
   state = {
-    gameState: 'new',
+    gameState: '',
     rows: 9,
-    columns: 9
+    columns: 9,
+    boardClass: 'small-board'
   }
 
   createBoard = () => {
@@ -24,11 +25,21 @@ class App extends React.Component {
     return board
   }
 
+  newGame = () => {
+
+  }
+
+  changeGameState = () => {
+    this.setState({
+      gameState: 'new'
+    })
+  }
+
   render(){
     return (
-      <div className="App">
-        <Header />
-        <Board board={this.createBoard()} rows={this.state.rows} columns={this.state.columns}/>
+      <div className={this.state.boardClass}>
+        <Header changeGameState={this.changeGameState} />
+        <Board board={this.createBoard()} rows={this.state.rows} columns={this.state.columns} />
       </div>
     )
   }
---
+++ src/components/GameButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GameButton(props){
+    return (
+        <button onClick={props.changeGameState}>🙂</button>
+    )
+}
