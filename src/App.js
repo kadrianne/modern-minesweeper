fbb5712ab061993b13f17aba8d5d13fedbe904b1
kadrianne <kristine.a.du@gmail.com>
2020-05-01 21:32:16 -0600

    Add timer functions.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Board from './components/Board'
 import GameButton from './components/GameButton'
 import FlagCounter from './components/FlagCounter'
@@ -9,6 +9,7 @@ function App() {
   const [gameState, setGameState] = useState('new')
   const [difficulty, setDifficulty] = useState('easy')
   const [flagsMarked, setFlagsMarked] = useState(0)
+  const [seconds,setSeconds] = useState(0)
   const [timerOn, setTimerOn] = useState(false)
 
   const config = {
@@ -22,25 +23,39 @@ function App() {
 
   const changeGameState = (gameState) => setGameState(gameState)
   const updateFlagsMarked = (flagsMarked) => setFlagsMarked(flagsMarked)
+  const startTimer = () => setTimerOn(true)
+  const stopTimer = () => setTimerOn(false)
+  const resetTimer = () => {
+    setSeconds(0)
+    setTimerOn(false)
+  }
 
   const displayText = () => {
-
     const text = {
       'won': '🎉 YOU WIN 🎉',
       'lost': '☠️ YOU LOSE ☠️'
     }
 
     return text[gameState]
-
   }
+
+  useEffect(() => {
+    let interval = null
+    if (timerOn == true){
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1)
+      }, 1000)
+    } 
+    return () => clearInterval(interval)
+  }, [timerOn,seconds])
   
   return (
     <>
     <div className={config[difficulty]['boardClass']}>
       <header>
           <FlagCounter difficulty={difficulty} flagsMarked={flagsMarked} />
-          <GameButton changeGameState={changeGameState} gameState={gameState} />
-          <Timer timerOn={timerOn} />
+          <GameButton changeGameState={changeGameState} gameState={gameState} resetTimer={resetTimer} />
+          <Timer timerOn={timerOn} seconds={seconds} />
       </header>
       <Board 
         rows={config[difficulty]['rows']}
@@ -50,6 +65,8 @@ function App() {
         gameState={gameState}
         difficulty={difficulty}
         updateFlagsMarked={updateFlagsMarked}
+        startTimer={startTimer}
+        stopTimer={stopTimer}
       />
     </div>
     <p class={gameState}>{displayText()}</p>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,8 @@
 import React, { useState,useEffect } from 'react'
 
-export default function Timer(){
-
-    const [seconds,setSeconds] = useState(0)
+export default function Timer(props){
 
     return (
-        <div class='timer'>⏱ {seconds}</div>
+        <div class='timer'>⏱ {props.seconds}</div>
     )
 }
