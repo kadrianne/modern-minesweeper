015e9649f879f76c760d4130c5fd754eb78c578d
kadrianne <kristine.a.du@gmail.com>
2020-05-06 09:45:54 -0600

    Create custom hook to be resuable for form.

---
+++ src/hooks/handleDialog.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+
+export default function useHandleDialog(){
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+        setOpen(true)
+    }
+    
+    const handleClose = () => {
+        setOpen(false)
+    }
+    
+    return [open,handleClick,handleClose]
+}
